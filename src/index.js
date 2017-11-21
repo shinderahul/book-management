855c695144945e43963dd15208a6265b99906aa9
Rahul Shinde <rahul.shinde@digitaslbi.com>
2017-11-21 15:20:23 +0530

    Added Stateless component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+import { data } from './data'
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Stateless Function
+const BookList = (props) => (
+	<ul>
+		{props.books.map(book => {
+			return (
+				<li key={book.id}>
+					{book.title} -- Rs.{book.price}
+				</li>
+				);
+		})}
+	</ul>
+);
+
+ReactDOM.render(
+	<BookList books={data.books} />,
+	document.getElementById('root')
+);
